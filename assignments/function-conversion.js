2ce1fa6921cffdf53c3ef827caabb963d821acd5
AdamMaley <adamemaley@yahoo.com>
2019-09-04 21:14:54 -0400

    test 2

--- assignments/function-conversion.js
+++ assignments/function-conversion.js
@@ -5,7 +5,6 @@
 // };
 // myFunction();
 
-<<<<<<< HEAD
 // let anotherFunction =  (param) => {
 //   return param;
 // };
@@ -19,15 +18,6 @@
 // let subtract = (param1, param2) => {
 //   return param1 - param2;
 // };
-=======
-// let anotherFunction = (param) => param;
-// anotherFunction("Example");
-
-// let add = (param1, param2) => param1 + param2;
-// add(1,2);
-
-// let subtract = (param1, param2) => param1 - param2;
->>>>>>> ab9714411a1f83bbd8e4e5ba19d3d5e5bfdc4359
 // subtract(1,2);
 
 
